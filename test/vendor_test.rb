880584a41e10ff03153867a78f18a35ff32618b7
Drew Williams <drewwilliams5280@gmail.com>
2020-07-29 10:06:23 -0500

    Sets up attribute test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,9 +8,11 @@ require './lib/vendor'
 
 class VendorTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_instance_of Vendor, vendor
+    assert_equal Hash.new, vendor.inventory
+    assert_equal "Rocky Mountain Fresh", vendor.name
   end
 
 
