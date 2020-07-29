21cf6d1c066ad09a7519a86422ed80ba5f30bac2
Drew Williams <drewwilliams5280@gmail.com>
2020-07-29 10:03:02 -0500

    Completes item exists test

---
+++ test/item_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+require './lib/vendor'
+
+
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_instance_of Item, item1
+  end
+
+
+end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -6,13 +6,8 @@ require './lib/vendor'
 
 
 
-class ItemTest < Minitest::Test
+class VendorTest < Minitest::Test
 
-  def test_it_exists
-    item1 = Item.new({name: 'Peach', price: "$0.75"})
-    item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    assert_instance_of Item, item1
-  end
 
 
 end
