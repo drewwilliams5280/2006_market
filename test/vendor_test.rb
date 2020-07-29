28a4efa28306ba635b285515663840de710aebc3
Drew Williams <drewwilliams5280@gmail.com>
2020-07-29 09:59:12 -0500

    Sets up exists test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -8,8 +8,11 @@ require './lib/vendor'
 
 class VendonTest < Minitest::Test
 
-
-
+  def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    assert_instance_of Item, item1
+  end
 
 
 end
