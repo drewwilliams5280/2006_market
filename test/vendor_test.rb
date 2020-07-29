d31d062d153e9d0f04a6dcf4d835776e132ead2b
Drew Williams <drewwilliams5280@gmail.com>
2020-07-29 10:08:47 -0500

    Adds check stock test setup

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -15,6 +15,14 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", vendor.name
   end
 
+  def test_it_can_check_stock
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal 0, vendor.check_stock(item1)
+
+  end
+
 
 
 end
