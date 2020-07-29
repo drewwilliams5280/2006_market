7528a82bb0a23240f7f7149f2ac523a581270703
Drew Williams <drewwilliams5280@gmail.com>
2020-07-29 10:23:18 -0500

    Sets up market test and files

---
+++ test/market_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+
+
+class MarketTest < Minitest::Test
+
+  def test_it_exists
+    market = Market.new("South Pearl Street Farmers Market")
+    assert_instance_of Market, market
+  end
+
+end
